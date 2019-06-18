208c92b7841a07c34c2677680151d49d3140e679
Yifei <yifeizhou.to@gmail.com>
2019-06-18 19:46:48 +0000

    Added migration files

---
+++ migrations/20190618190002_create_milestones.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('milestones', function(table){
+      table.increments('id');
+      table.string('description');
+      table.date('date_achieved');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .dropTable('milestones');
+};
